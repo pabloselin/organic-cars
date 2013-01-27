f02de40ce572aff9c4e106a299b13a03737349a7
Anathorn <vcarrascoing@hotmail.com>
2013-01-27 18:59:50 -0300

    just test files

---
+++ Scripts/conf.lua
@@ -0,0 +1,5 @@
+function love.conf(a)
+	a.title = "Hello World Example"
+	a.screen.width = 900
+	a.screen.height = 900
+end
---
+++ Scripts/main.lua
@@ -0,0 +1,16 @@
+require "player.lua"
+
+function love.load()
+
+	smallFont = love.graphics.newFont(30)
+	mediumFont = love.graphics.newFont(45)
+	bigFont = love.graphics.newFont(70)
+end
+
+function love.update(dt)
+	playerMove(dt)
+end
+
+function love.draw()
+	playerDraw()
+end
